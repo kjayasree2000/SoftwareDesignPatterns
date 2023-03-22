f397694c704715278028540aa925cb22d7ee067f
Jayasree Ammanabrolu <jayasreeammanabrolu@live.com>
2023-03-22 14:27:44 -0700

    My First Commit

---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/DependencyInjection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    class DependencyInjection
+    {
+        // Constructor InJection
+        readonly SUV suvobj = new SUV();
+        readonly Compact comobj = new Compact();
+
+        public void PrintCar(ICAR obj)
+        {
+            obj.GetCost();
+            obj.GetColor();
+        }     
+
+    }
+
+    interface ICAR
+    {
+        void GetCost();
+        void GetColor();
+    }
+
+    public class SUV : ICAR
+    {
+        public void GetCost()
+        {
+            Console.WriteLine("Cost of SUV is $50K");            
+        }
+
+        public void GetColor()
+        {
+            Console.WriteLine("Available color is Blue");
+        }
+    }
+
+    public class Compact : ICAR
+    {
+        public void GetCost()
+        {
+            Console.WriteLine("Cost of Compact is $30K");
+        }
+
+        public void GetColor()
+        {
+            Console.WriteLine("Available Color is Grey");
+        }
+    }
+}
---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DependencyInjection ds = new DependencyInjection();
+            SUV suvcar = new SUV();
+            Compact comcar = new Compact();
+            //Constructor Injection
+            ds.PrintCar(suvcar);
+            ds.PrintCar(comcar);
+            Console.ReadLine();
+
+        }
+    }
+}
