c96f2a8aa4c2d123c460ad7278884649a0751b02
Jayasree Ammanabrolu <jayasreeammanabrolu@live.com>
2023-06-06 23:45:58 -0700

    Pub Sub Pattern demo

---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/MyEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    public class MyEventArgs: EventArgs
+    {
+        public int Value { get; set; }
+
+        public MyEventArgs(int Value)
+        {
+            this.Value = Value;
+        }
+
+    }
+}
---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/Pub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    public class Pub
+    {
+        public event EventHandler<MyEventArgs> onchange = delegate { };
+
+        public void Raise()
+        {
+            if (onchange != null)
+            {
+                onchange(this, new MyEventArgs(33));
+            }
+        }
+
+    }
+}
