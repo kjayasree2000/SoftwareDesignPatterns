9a9801efbaab005c80a336e2b559a2dcc995127e
kjayasree2000 <31083110+kjayasree2000@users.noreply.github.com>
2024-02-16 12:58:31 -0800

    Add files via upload

--- SoftwareDesignPatterns/SoftwareDesignPatterns/Program.cs
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/Program.cs
@@ -10,13 +10,21 @@ namespace SoftwareDesignPatterns
     {
         static void Main(string[] args)
         {
-            DependencyInjection ds = new DependencyInjection();
-            SUV suvcar = new SUV();
-            Compact comcar = new Compact();
-            //Constructor Injection
-            ds.PrintCar(suvcar);
-            ds.PrintCar(comcar);
+            ////DependencyInjection ds = new DependencyInjection();
+            ////SUV suvcar = new SUV();
+            ////Compact comcar = new Compact();
+            //////Constructor Injection
+            ////ds.PrintCar(suvcar);
+            ////ds.PrintCar(comcar);
+            ////Console.ReadLine();
+            ///
+
+
+            string vehiclename = Console.ReadLine();
+            VehicleFactory vf = new VehicleFactory();
+            Console.WriteLine(vf.GetVehicle(vehiclename).NumberofWheels());
             Console.ReadLine();
+            
 
         }
     }
