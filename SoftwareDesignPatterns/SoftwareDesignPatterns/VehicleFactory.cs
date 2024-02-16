f56205dcabe5cee217a522b748b48ddfc157a3ea
kjayasree2000 <31083110+kjayasree2000@users.noreply.github.com>
2024-02-16 12:57:10 -0800

    Add files via upload

---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/VehicleFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    public class VehicleFactory 
+    {
+        IVehicle objecttype = null;        
+
+        public IVehicle GetVehicle(string vehicleName)
+        {
+            if (vehicleName.ToLower().Equals("car")){
+                objecttype = new Car();
+            }
+
+            else if (vehicleName.ToLower().Equals("bike"))
+            {
+                objecttype = new Bike();
+            }
+
+            else if (vehicleName.ToLower().Equals("rickshaw"))
+            {
+                objecttype = new RickShaw();
+            }
+
+            return objecttype;
+
+        }
+
+    }
+}
