c52e65f6da247c1168d79774177862e404c60a17
kjayasree2000 <31083110+kjayasree2000@users.noreply.github.com>
2024-02-16 12:56:10 -0800

    Add files via upload

---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/IVehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    public interface IVehicle
+    {
+         string VehicleType();
+         int NumberofWheels();
+    }
+}
