1c85b391641bdf571339f5f7424b112c4c3fe105
kjayasree2000 <31083110+kjayasree2000@users.noreply.github.com>
2024-02-16 12:57:41 -0800

    Add files via upload

---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/RickShaw.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    public class RickShaw : IVehicle
+    {
+        private readonly int wheels;
+
+        public RickShaw()
+        {
+            this.wheels = 3;
+        }
+
+        public int NumberofWheels()
+        {
+            return wheels;
+        }
+
+        public string VehicleType()
+        {
+            return "RickShaw Class";
+        }
+
+    }
+}
