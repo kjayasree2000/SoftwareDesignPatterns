f7488f9c3f6d937c917c5e453f57bdfa6d63d1ef
kjayasree2000 <31083110+kjayasree2000@users.noreply.github.com>
2024-02-16 12:53:22 -0800

    Add files via upload

---
+++ SoftwareDesignPatterns/Bike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    public class Bike : IVehicle
+    {
+        private readonly int wheels;
+
+        public Bike()
+        {
+            this.wheels = 2;
+        }
+
+        public int NumberofWheels()
+        {
+            return this.wheels;
+        }
+
+        public string VehicleType()
+        {
+            return "bike class";
+        }
+    }
+}
