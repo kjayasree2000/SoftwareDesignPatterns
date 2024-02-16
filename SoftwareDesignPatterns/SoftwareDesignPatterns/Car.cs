4fa368ac76cc409fb78def5ced30ce28455c47a9
kjayasree2000 <31083110+kjayasree2000@users.noreply.github.com>
2024-02-16 12:56:35 -0800

    Add files via upload

---
+++ SoftwareDesignPatterns/SoftwareDesignPatterns/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns
+{
+    public class Car : IVehicle
+    {
+        private readonly int wheels;
+
+        public Car()
+        {
+            this.wheels = 4;
+        }
+
+        public int NumberofWheels()
+        {
+            return wheels;
+
+        }
+        public string VehicleType()
+        {
+            return "Car class";
+        }
+
+    }
+}
